2e1d50d55c9e0fbb56ec5256df8ecb7d104970be
MedicineMan94 <59082390+MedicineMan94@users.noreply.github.com>
2020-03-18 15:38:31 +0100

    volgend scherm

---
+++ app/src/main/java/com/example/onboarding/Main/VolgendScherm.java
@@ -0,0 +1,16 @@
+package com.example.onboarding.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onboarding.R;
+
+public class VolgendScherm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volgend_scherm);
+    }
+}

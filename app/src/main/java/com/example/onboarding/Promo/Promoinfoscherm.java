cdb0293f27dea458d510f5e13cce0fb65c039d87
joep1243 <31277643+joep1243@users.noreply.github.com>
2020-03-19 15:54:19 +0100

    promo scherm
    info en promoscherm aangemaakt en intent

---
+++ app/src/main/java/com/example/onboarding/Promo/Promoinfoscherm.java
@@ -0,0 +1,19 @@
+package com.example.onboarding.Promo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.onboarding.R;
+
+public class Promoinfoscherm extends AppCompatActivity {
+
+
+
+
+
+
+}

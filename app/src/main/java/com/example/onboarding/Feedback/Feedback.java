7b0b078f021d1629fe67dd5539e18360ec8b89d4
luukwitters <56395250+luukwitters@users.noreply.github.com>
2020-03-21 15:42:37 +0100

    Feedbackscherm
    Er wordt gecheckt of 1 van de 2 keuzes oneens is. Knop is niet clickable als txt leeg is.

--- app/src/main/java/com/example/onboarding/Feedback/Feedback.java
+++ app/src/main/java/com/example/onboarding/Feedback/Feedback.java
@@ -3,12 +3,21 @@ package com.example.onboarding.Feedback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.onboarding.R;
+import com.example.onboarding.helpers.VolleyHelper;
 
 public class Feedback extends AppCompatActivity {
 
@@ -21,6 +30,7 @@ public class Feedback extends AppCompatActivity {
     private TextView txtIntake;
     private TextView txtOpenDag;
     private TextView txtOpenVraag;
+    private int iTeller = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +47,29 @@ public class Feedback extends AppCompatActivity {
         txtOpenDag = findViewById(R.id.txtOpendag);
         txtOpenVraag = findViewById(R.id.txtOpenVraag);
 
+        txtFeedback.addTextChangedListener(feedbackTextWatcher);
+
     }
+
+    private TextWatcher feedbackTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String feedbackInput = txtFeedback.getText().toString().trim();
+
+            btnNext.setEnabled(!feedbackInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
 
     public void IntakeEens(View v) {
         System.out.println("Eens Intake");
@@ -58,31 +90,43 @@ public class Feedback extends AppCompatActivity {
         btnOpenOneens.setVisibility(View.VISIBLE);
         txtIntake.setVisibility(View.INVISIBLE);
         txtOpenDag.setVisibility(View.VISIBLE);
+        iTeller=1;
     }
 
     public void OpenEens(View v) {
         System.out.println("Eens Open");
-        btnOpenEens.setVisibility(View.INVISIBLE);
-        btnOpenOneens.setVisibility(View.INVISIBLE);
-        txtFeedback.setVisibility(View.VISIBLE);
-        txtOpenDag.setVisibility(View.INVISIBLE);
-        txtOpenVraag.setVisibility(View.VISIBLE);
-        btnNext.setVisibility(View.VISIBLE);
+
+        if (iTeller==0){
+            System.out.println("Next Screen");
+            btnOpenEens.setVisibility(View.INVISIBLE);
+            btnOpenOneens.setVisibility(View.INVISIBLE);
+            txtOpenDag.setVisibility(View.INVISIBLE);
+        }
+        else {
+            txtOpenVraag.setVisibility(View.VISIBLE);
+            txtFeedback.setVisibility(View.VISIBLE);
+            btnNext.setVisibility(View.VISIBLE);
+            btnOpenEens.setVisibility(View.INVISIBLE);
+            btnOpenOneens.setVisibility(View.INVISIBLE);
+            txtOpenDag.setVisibility(View.INVISIBLE);
+        }
+
     }
 
     public void OpenOneens(View v) {
         System.out.println("Oneens Open");
+        txtOpenVraag.setVisibility(View.VISIBLE);
+        txtFeedback.setVisibility(View.VISIBLE);
+        btnNext.setVisibility(View.VISIBLE);
         btnOpenEens.setVisibility(View.INVISIBLE);
         btnOpenOneens.setVisibility(View.INVISIBLE);
-        txtFeedback.setVisibility(View.VISIBLE);
         txtOpenDag.setVisibility(View.INVISIBLE);
-        txtOpenVraag.setVisibility(View.VISIBLE);
-        btnNext.setVisibility(View.VISIBLE);
-    }
+        }
 
     public void Verder (View v) {
         System.out.println("Button works");
     }
 
-
 }
+
+

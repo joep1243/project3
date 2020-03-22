3e84f133a19bd59b99e485802d21cadfc326210e
joep1243 <31277643+joep1243@users.noreply.github.com>
2020-03-22 01:29:11 +0100

    update promoscherm
    button heeft nu een timer delay voor zijn visibility

--- app/src/main/java/com/example/onboarding/Promo/Promoscherm.java
+++ app/src/main/java/com/example/onboarding/Promo/Promoscherm.java
@@ -7,18 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.onboarding.Model.PromoModel;
 import com.example.onboarding.R;
 
 
 public class Promoscherm extends AppCompatActivity {
+
+    private PromoModel PromoModel;
+
     private Button btnPinfo;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.promoscherm);
-
-
     }
 
     public void openinfoPromo(View v){

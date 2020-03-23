67c2b316a38c21ba3dfc4aa35c3cbc643b3c06a9
luukwitters <56395250+luukwitters@users.noreply.github.com>
2020-03-23 15:05:37 +0100

    Update Feedback.java

--- app/src/main/java/com/example/onboarding/Feedback/Feedback.java
+++ app/src/main/java/com/example/onboarding/Feedback/Feedback.java
@@ -7,19 +7,11 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.onboarding.R;
 import com.example.onboarding.helpers.VolleyHelper;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 

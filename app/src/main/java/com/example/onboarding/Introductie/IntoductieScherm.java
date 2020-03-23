9ab26a269feb763a5d8a4c48d41a17982858fccf
marfmans <60438820+marfmans@users.noreply.github.com>
2020-03-23 10:50:23 +0100

    introductiescherm
    introscherm aangemaakt

---
+++ app/src/main/java/com/example/onboarding/Introductie/IntoductieScherm.java
@@ -0,0 +1,14 @@
+package com.example.onboarding.Introductie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntoductieScherm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intoductie_scherm);
+    }
+}

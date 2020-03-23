1bfd4d7f4795e933155e3c36f3e2cf5e8660ec6d
joep1243 <31277643+joep1243@users.noreply.github.com>
2020-03-23 19:57:49 +0100

    email functie test

--- app/src/main/java/com/example/onboarding/Promo/Promoscherm.java
+++ app/src/main/java/com/example/onboarding/Promo/Promoscherm.java
@@ -3,9 +3,11 @@ package com.example.onboarding.Promo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.onboarding.Model.PromoModel;
 import com.example.onboarding.R;
@@ -28,11 +30,29 @@ public class Promoscherm extends AppCompatActivity {
         //PromoModel PromoModel = new PromoModel();
 
         btnPinfo = findViewById(R.id.btnPinfo);
+        try {
+            PromoModel.GetPtext();
+        }catch (Exception e){
 
+            System.out.println(e);
+        }
     }
 
     public void openinfoPromo(View v){
         try {
+
+            /*Intent i = new Intent(Intent.ACTION_SENDTO);
+            i.setType("message/rfc822");
+            i.setData(Uri.parse("mailto: luuk.witters@gmail.com"));
+            //i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, "Test mail");
+            i.putExtra(Intent.EXTRA_TEXT   , "kan nu mailen via de app");
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(Promoscherm.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }*/
+
             PromoModel.GetPtext();
         }catch (Exception e){
 

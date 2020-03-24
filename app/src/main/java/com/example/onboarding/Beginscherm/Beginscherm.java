76fddd8f110511a7321eefa597cd34bdd05f9211
thelightbearer <rickmertens1998@gmail.com>
2020-03-25 00:09:05 +0100

    Update volleyhelper

--- app/src/main/java/com/example/onboarding/Beginscherm/Beginscherm.java
+++ app/src/main/java/com/example/onboarding/Beginscherm/Beginscherm.java
@@ -1,5 +1,6 @@
 package com.example.onboarding.Beginscherm;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +30,7 @@ import org.json.JSONObject;
 public class Beginscherm extends AppCompatActivity {
 
     private Button btnbegin;
+    private android.content.Context Context;
 
 
     @Override
@@ -36,6 +38,35 @@ public class Beginscherm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beginscherm);
 
+        VolleyHelper secondHelper = new VolleyHelper(Context, "https://adaonboarding.ml/t3/OnboardingAPI/GetTEXT");
+        secondHelper.get("index.php", null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+
+                    JSONObject jsonObject = new JSONObject (response.toString());
+
+                    String opleiding = jsonObject.getString("opleidingen");
+                    JSONObject jsonObject1 = new JSONObject (opleiding);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Locale error handlin
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
 
     }
 }

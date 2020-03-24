55328e0c31d33a2d4f753e4e92096900d0728306
MedicineMan94 <59082390+MedicineMan94@users.noreply.github.com>
2020-03-24 21:47:53 +0100

    Merge met vragenscherm Joep

--- app/src/main/java/com/example/onboarding/Model/Code.java
+++ app/src/main/java/com/example/onboarding/Model/Code.java
@@ -1,7 +1,12 @@
-package Model;
+package com.example.onboarding.Model;
 
+/**
+ *
+ */
 public class Code {
     String StudentID = "";
+    String StudentKlas = "";
     Integer QID = 0;
     Boolean ESE = Boolean.FALSE;
+
 }
---
+++ app/src/main/java/com/example/onboarding/Promo/Promoscherm.java
@@ -0,0 +1,73 @@
+package com.example.onboarding.Promo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.onboarding.Model.PromoModel;
+import com.example.onboarding.R;
+
+
+public class Promoscherm extends AppCompatActivity {
+    private PromoModel PromoModel;
+
+    private TextView txtPromoVraag;
+
+    private Button btnPinfo;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.promoscherm);
+        btnPinfo = findViewById(R.id.btnPinfo);
+        txtPromoVraag = findViewById(R.id.txtPromoVraag);
+
+        PromoModel PromoModel = new PromoModel();
+
+        PromoModel.GetPtext(getBaseContext(), "Vraag");
+
+
+
+        try {
+
+        }catch (Exception e){
+
+            System.out.println(e);
+        }
+    }
+
+    public void openinfoPromo(View v){
+        try {
+
+            /*Intent i = new Intent(Intent.ACTION_SENDTO);
+            i.setType("message/rfc822");
+            i.setData(Uri.parse("mailto: luuk.witters@gmail.com"));
+            //i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, "Test mail");
+            i.putExtra(Intent.EXTRA_TEXT   , "kan nu mailen via de app");
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(Promoscherm.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }*/
+
+        }catch (Exception e){
+
+            System.out.println(e);
+        }
+
+        //Intent intent = new Intent(this, Promoinfoscherm.class);
+       // startActivity(intent);
+    }
+
+
+
+}

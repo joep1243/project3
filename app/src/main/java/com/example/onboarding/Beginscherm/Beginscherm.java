2200be80954d0ae4c3205a138d5885734451ec58
thelightbearer <rickmertens1998@gmail.com>
2020-03-26 16:42:01 +0100

    Update

--- app/src/main/java/com/example/onboarding/Beginscherm/Beginscherm.java
+++ app/src/main/java/com/example/onboarding/Beginscherm/Beginscherm.java
@@ -1,6 +1,8 @@
 package com.example.onboarding.Beginscherm;
 
+import android.os.AsyncTask;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,6 +22,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.onboarding.Main.Vraagscherm;
 import com.example.onboarding.R;
 import com.example.onboarding.helpers.VolleyHelper;
 
@@ -26,55 +30,69 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 
-public class Beginscherm extends AppCompatActivity {
+
+public class Beginscherm<$mysql_user> extends AppCompatActivity {
 
     private Button btnbegin;
     private android.content.Context Context;
+    private EditText StudentID;
+    private TextView txtwelkom;
+    private static final String url = "http://localhost/phpmyadmin/db_structure.php?server=1&db=onboarding";
+    private static final String user = "root";
+    private static final String pass = "root";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beginscherm);
-        btnbegin = findViewById(R.id.btnbegin);
 
+        StudentID = (EditText) findViewById(R.id.StudentID);
+        btnbegin = (Button) findViewById(R.id.btnbegin);
+        txtwelkom = (TextView) findViewById(R.id.txtwelkom);
 
-        VolleyHelper secondHelper = new VolleyHelper(getBaseContext(), "https://adaonboarding.ml/t3/OnboardingAPI/GetTEXT");
-        secondHelper.get("index.php", null, new Response.Listener<JSONObject>() {
+//        btnbegin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+ //               if (validate()) {
+//
 
-            @Override
-            public void onResponse(JSONObject response) {
+                    // db check true/false hier
 
-                try {
-
-                    JSONObject jsonObject = new JSONObject (response.toString());
-                    String opleidingen = jsonObject.getString("opleidingen");
-
-                    for (int i = 0; i < jsonObject.length(); i++) {
-                        JSONObject Opleiding = jsonObject.getJSONObject(String.valueOf(i));
-
-                        String opleiding = Opleiding.getString("opleiding");
-
-                    }
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    //SELECT * FROM studentminuutdatumvraag WHERE sID = "%s"', $SID
+                    //INSERT INTO studentminuutdatumvraag VALUES(StudentID, current_timestamp(), NULL, NULL)';
                 }
+//            }
+//        });
+//    }
+//    private void setupUIViews() {
+//        StudentID = (EditText) findViewById(R.id.StudentID);
+//        btnbegin = (Button) findViewById(R.id.btnbegin);
+//    }
 
+//    private Boolean validate( ){
+//        Boolean result = false;
+//        String student = StudentID.getText().toString();
+//       if(student.isEmpty()) {
+//            Toast.makeText(this, "Vul je student id in", Toast.LENGTH_SHORT).show();
+//        }else{
+//            result = true;
+//        }
+//        return result;
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // Locale error handlin
-            }
-        });
 
     }
-    @Override
-    public void onBackPressed() {
 
-    }
-}
+
+//        @Override
+//        protected void onPostExecute(String result) {
+//            txtwelkom.setText(result);
+//        }
+//    }
+//}
